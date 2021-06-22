c5c67c697ec5f3fb4f3750a623c954f50091aa73
rithgroove <rithgroove@gmail.com>
2021-06-22 16:19:31 +0900

    try to create infection model

---
+++ lib/Simulation/Infection.py
@@ -0,0 +1,10 @@
+
+def infect(simulator):
+    for x in simulator.agent:
+        node = x.currentNode
+        otherAgents = []
+        for node in node.connections:
+            for other agents = node.agents:
+                #check if agent position is within a certain number of meter
+                include to the other agents
+                
