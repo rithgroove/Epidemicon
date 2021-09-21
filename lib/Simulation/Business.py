c2c752a82d0588092a3b5ad257bded497f9973a1
k1 <fhktanaka@gmail.com>
2021-09-21 12:19:22 +0900

    hotfix workdays

--- lib/Simulation/Business.py
+++ lib/Simulation/Business.py
@@ -37,7 +37,6 @@ class Business:
             activityPerWeek =  rng.integers(min_activity_per_week, max_activity_per_week)            
         activityPerWeek = np.min([activityPerWeek, len(workdays)])
         self.workdays = rng.choice(workdays, activityPerWeek, replace=False)
-        self.workdays.sort()
 
         # These vars are used to set the lockdown and reset it to the original value when it ends
         self.isLockdown = False
