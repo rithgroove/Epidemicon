55642157d55d8682d1a86bfb75a92d08439ac6f2
rithgroove <rithgroove@gmail.com>
2021-09-24 13:49:41 +0900

    deleted files

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+nohup python main_no_render.py -c config4.yml --no_infectious_stop > output4.txt &
+nohup python main_no_render.py -c config6.yml --no_infectious_stop > output6.txt &
+wait
