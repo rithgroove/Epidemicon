24a7ad2bda6d2dec2a38b68be3205ac1835d7f3f
k1 <fhktanaka@gmail.com>
2021-09-27 17:51:24 +0900

    config files

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-nohup python main_no_render.py -c config4.yml --no_infectious_stop > output4.txt &
-nohup python main_no_render.py -c config6.yml --no_infectious_stop > output6.txt &
+python main_no_render.py -c config.yml --no_infectious_stop > output1.txt &
+python main_no_render.py -c config2.yml --no_infectious_stop > output2.txt &
+python main_no_render.py -c config3.yml --no_infectious_stop > output3.txt &
+wait
