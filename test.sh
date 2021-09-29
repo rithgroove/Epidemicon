80f4b76200fd15751631757d83a2c0f37e288667
Jair Pereira Junior <pereira-junior.ua.ws@alumni.tsukuba.ac.jp>
2021-09-30 01:49:32 +0900

    set up files

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 nohup python main_no_render.py -c config1.yml --no_infectious_stop > output1.txt &
+nohup python main_no_render.py -c configA.yml --no_infectious_stop > outputA.txt &
 wait
